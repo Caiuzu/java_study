2c6a4d00ae68afaa1f99bfb7e41cf2bae412fd58
Caio Souza <caio.7s@outlook.com>
2020-08-05 00:23:59 -0300

    Aula 7 - Controlando Fluxo com Laços

---
+++ Java I/Aula7ControlandoFluxoComLacos/src/MultiploDe3.java
@@ -0,0 +1,9 @@
+public class MultiploDe3 {
+    public static void main(String[] args) {
+        for (int numero = 0; numero <= 100; numero++) {  // for (int i = 3; i < 100; i += 3 ){
+            if ((numero % 3) == 0) {
+                System.out.println(numero);
+            }
+        }
+    }
+}
