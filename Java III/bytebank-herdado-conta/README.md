215f2bf155186da95f8f3498f41585b29fac6b5c
Caio Souza <caio.7s@outlook.com>
2021-01-23 02:52:57 -0300

    Aula 4 - Herança e o uso de construtores

---
+++ Java III/bytebank-herdado-conta/README.md
@@ -0,0 +1,12 @@
+# Java Polimorfismo: Entenda herança e interfaces
+
+---
+
+### Aula 4 - Herança e o uso de construtores:
+
+- **Conceitos de herança, construtores e polimorfismo:**
+    - Quando uma classe herda de outra ela recebe apenas seus atributos e métodos;
+    - Construtores não são herdados;
+- **A utilização da anotação `@Override`:**
+    - É usada para sobrescrever o método da classe mãe, indicando que o método original foi alterado;
+- **Um construtor da classe mãe pode ser chamado através do `super()`**.
---
+++ Java III/bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,90 @@
+public class Conta {
+
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    private static int total;
+
+    public Conta(int agencia, int numero) {
+        Conta.total++;
+        this.setAgencia(agencia);
+        this.setNumero(numero);
+        //System.out.println("estou criando uma conta, total de contas: " + Conta.total);
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        if (agencia <= 0) {
+            System.out.println("Valor inválido");
+            return;
+        }
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        if (agencia <= 0) {
+            System.out.println("Valor inválido");
+            return;
+        }
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public void deposita(final double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (verificaSaldo(valor)) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (verificaSaldo(valor)) {
+            destino.deposita(valor);
+            this.saca(valor);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verificaSaldo(double valor) {
+        return this.saldo >= valor;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
+    }
+
+    public static void setTotal(int total) {
+        Conta.total = total;
+    }
+
+}
